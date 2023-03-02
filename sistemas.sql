9e10229640a7c3444cf2004e3d3a9c8f311a104d
alejopro69 <alejandroguzmangonzalez@gimnasiocalibio.edu.co>
2023-03-02 11:54:02 -0500

    agrego documentacion

---
+++ sistemas.sql
@@ -0,0 +1,27 @@
+create database GimnasioCalibio;
+
+create table Estudiante(
+nombre varchar(50),
+Pk_codigoE int,
+fecha_nacimiento date
+);
+
+create table Profesor(
+nombre varchar(50)
+Pk_codigo_profesor int,
+fecha_nacimiento date
+id int,
+);
+
+create table Asignaturas(
+nombre varchar(50),
+Pk_codigoA int;
+horario date
+);
+
+ins}alter into Estudiante (nombre; Pk_codigoE, fecha_nacimiento) values (
+"jose", 12345, "2008/09/15" 
+)
+
+
+
